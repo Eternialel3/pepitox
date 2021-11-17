87ea3677c03eb6c39fee7ae1808804edf4438b9c
Eternialel3 <94089822+Eternialel3@users.noreply.github.com>
2021-11-17 15:22:06 +0100

    Add files via upload

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -16,13 +16,13 @@ module.exports = {
 
             const success = queue.setRepeatMode(queue.repeatMode === 0 ? QueueRepeatMode.QUEUE : QueueRepeatMode.OFF);
 
-            return message.reply(success ? `La loop de la queue est **${queue.repeatMode === 0 ? 'désactivée' : 'activée'}**.` : `Quelque chose s'est mal passée.`);
+            return message.reply(success ? `La loop de la file est **${queue.repeatMode === 0 ? 'désactivée' : 'activée'}**.` : `Quelque chose s'est mal passée.`);
         } else {
-            if (queue.repeatMode === 2) return message.reply(`Tu dois d'abord désactiver la loop de la queue (${client.config.app.px}loop queue).`);
+            if (queue.repeatMode === 2) return message.reply(`Tu dois d'abord désactiver la loop de la file (${client.config.app.px}loop queue).`);
 
             const success = queue.setRepeatMode(queue.repeatMode === 0 ? QueueRepeatMode.TRACK : QueueRepeatMode.OFF);
 
-            return message.reply(success ? `La loop de la musique est **${queue.repeatMode === 0 ? 'désactivée' : 'activée'}**. Pour répéter la queue entière utilisez (-loop queue).` : `Quelque chose s'est mal passée.`);
+            return message.reply(success ? `La loop de la musique est **${queue.repeatMode === 0 ? 'désactivée' : 'activée'}**. Pour répéter la file entière utilisez (-loop queue).` : `Quelque chose s'est mal passée.`);
         };
     },
 };
