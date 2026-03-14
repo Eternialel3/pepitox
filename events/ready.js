fec410bf0a62b24e3f768558cf94b2dc40382c7f
Math31415 <84249915+Math31415@users.noreply.github.com>
2026-03-14 14:38:19 +0100

    Delete events directory

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (client) => {
-    console.log(`Prêt à diffamer les connards ^^`);
-
-    client.on('error', console.error); // Afficher les erreurs
-
-    client.user.setActivity(client.config.app.playing);
-};
